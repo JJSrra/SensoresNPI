40b64fab07fd401645188ffba1a02d847cc10a47
Alberto Armijo Ruiz <armijoalb@correo.ugr.es>
2018-01-15 19:11:51 +0100

    Sensores cambiados

--- app/src/main/java/io/github/jjsrra/sensoresnpi/MainActivity.java
+++ app/src/main/java/io/github/jjsrra/sensoresnpi/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,10 +14,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import java.io.DataOutputStream;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -26,25 +28,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView girox;
     private TextView giroy;
     private TextView giroz;
-    private TextView aceleracionx;
-    private TextView aceleraciony;
-    private TextView aceleracionz;
     private TextView multitouch;
     private RelativeLayout background;
     private boolean boton_pulsado;
     SensorManager mSensorManager;
     Sensor giroscopio;
     Sensor acelerometro;
+    Sensor rotationSensor;
     private Button button, resetButton;
     private int touch_position_y;
     private int touch_current_position_y;
     private float global_x, global_y, global_z, current_x, current_y, current_z;
 
-    private static String IP_SERVER = "192.168.0.162";
-    private static int SERVER_PORT = 4000;
-    private Socket s;
-    private DataOutputStream dos;
-    private PrintWriter pw;
+    private connectTask mConnection;
+    private tcpClient mTcpClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         acelerometro = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         giroscopio = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        rotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
         boton_pulsado = false;
 
         girox = findViewById(R.id.GiroscopioX);
@@ -60,9 +61,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         giroz = findViewById(R.id.GiroscopioZ);
         button = findViewById(R.id.button);
         resetButton = findViewById(R.id.resetMultitouch);
-        aceleracionx = findViewById(R.id.AcelerometroX);
-        aceleraciony = findViewById(R.id.AcelerometroY);
-        aceleracionz = findViewById(R.id.AcelerometroZ);
         background = findViewById(R.id.background);
         multitouch = findViewById(R.id.multitouch);
 
@@ -75,6 +73,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         touch_position_y = 0;
         touch_current_position_y = 0;
+
+        mConnection = new connectTask();
+        mConnection.execute();
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -99,7 +100,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
-        new Thread(new ClientThread()).start();
 
     }
 
@@ -121,9 +121,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     int diff = touch_position_y - touch_current_position_y;
                     if (diff < -300)
                         multitouch.setText("¡Deslizado con 2 dedos!\nPulsa el botón \"Reiniciar\" para volver a probar");
-                        global_x = 0;
-                        global_y = 0;
-                        global_z = 0;
+                        girox.setText("x = 0");
+                        giroy.setText("y = 0");
+                        giroz.setText("z = 0");
+
                     break;
             }
         }
@@ -136,63 +137,60 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume(){
         super.onResume();
-        mSensorManager.registerListener(this, giroscopio, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+        // Lo registramos.
+        mSensorManager.registerListener(this,rotationSensor,mSensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+        mConnection.cancel(true);
     }
 
-    protected void sendMessage(String msg){
-        pw.write(msg);
-        //pw.flush();
-    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
         if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
-
-            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                girox.setText(R.string.act_main_no_accuracy);
-                giroy.setText(R.string.act_main_no_accuracy);
-                giroz.setText(R.string.act_main_no_accuracy);
-            }
-
-            if(s != null){
-                sendMessage("Esto no funciona compaeo");
-            }
-            return;
+            girox.setText(R.string.act_main_no_accuracy);
+            giroy.setText(R.string.act_main_no_accuracy);
+            giroz.setText(R.string.act_main_no_accuracy);
         }
-
         if (boton_pulsado) {
-            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                current_x = event.values[0];
-                current_y = event.values[1];
-                current_z = event.values[2];
+            // Creamos una matriz de rotation con los valores obtenidos de los values del sensor.
+            float[] rotationMatrix = new float[16];
+            mSensorManager.getRotationMatrixFromVector(rotationMatrix,event.values);
 
-                global_x = roundValues(global_x+current_x, 2);
-                global_y = roundValues(global_y+current_y, 2);
-                global_z = roundValues(global_z+current_z, 2);
+            // Remap coordenadas.
+            float[] remapRotationMatrix = new float[16];
+            mSensorManager.remapCoordinateSystem(rotationMatrix,
+                    mSensorManager.AXIS_X,
+                    mSensorManager.AXIS_Z,
+                    remapRotationMatrix);
 
-                girox.setText("x = " + global_x);
-                giroy.setText("y = " + global_y);
-                giroz.setText("z = " + global_z);
+            // Obtenemos la orientación y convertimos las orientaciones a grados.
+            float[] orientaciones = new float[3];
+            mSensorManager.getOrientation(remapRotationMatrix,orientaciones);
 
-                String giroMsg = global_x+"|"+global_y+"|"+global_z;
-
-                Log.e("TCP client","trying to connect");
+            for(int i=0; i < 3; i++){
+                orientaciones[i] = (float) (Math.toDegrees(orientaciones[i]));
             }
 
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                aceleracionx.setText("x = " + Float.toString(event.values[0]));
-                aceleraciony.setText("y = " + Float.toString(event.values[1]));
-                aceleracionz.setText("z = " + Float.toString(event.values[2]));
+            int aux, auy, auz;
+            aux = (int) orientaciones[0];
+            auy = (int) orientaciones[1];
+            auz = (int) orientaciones[2];
 
-            }
+            girox.setText( "x = " + aux );
+            giroy.setText( "y = " + auy );
+            giroz.setText( "z = " + auz );
+
+            String GiroMsg = aux+"|"+auy+"|"+"|"+auz+"\n";
+            mTcpClient.sendMessage(GiroMsg);
+
+            return;
         }
 
     }
@@ -206,27 +204,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return bd.floatValue();
     }
 
-    class ClientThread implements Runnable {
+    class connectTask extends AsyncTask<Void, Void, Void> {
 
         @Override
-        public void run() {
+        protected Void doInBackground(Void... msg){
+            mTcpClient = new tcpClient();
+            mTcpClient.runClient();
 
-            try {
-                InetAddress serverAddr = InetAddress.getByName(IP_SERVER);
-
-                s = new Socket(serverAddr, SERVER_PORT);
-                pw = new PrintWriter(s.getOutputStream());
-
-                Log.println(Log.VERBOSE,"socket","conectado al server");
-
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-
+            return null;
         }
 
+        @Override
+        protected void onCancelled(){
+            super.onCancelled();
+            if(mTcpClient != null) {
+                mTcpClient.stopClient();
+            }
+        }
+
+
     }
+
 }
 
---
+++ app/src/main/java/io/github/jjsrra/sensoresnpi/tcpClient.java
@@ -0,0 +1,76 @@
+package io.github.jjsrra.sensoresnpi;
+
+import android.util.Log;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Alberto on 15/01/2018.
+ */
+
+public class tcpClient {
+    private static String IP_SERVER = "192.168.1.6";
+    private static int SERVER_PORT = 5000;
+
+    private PrintWriter pw;
+    private Socket mSocket;
+    private Boolean run = true;
+
+
+    public void stopClient(){
+        run = false;
+        try {
+            mSocket.close();
+            pw.close();
+        }catch (IOException e){
+
+        }
+    }
+
+    public void sendMessage(String msg){
+        if( pw != null && !pw.checkError() ){
+            new sendMessageThread(msg).start();
+        }
+    }
+
+    public void runClient(){
+        run = true;
+
+        try {
+            InetAddress serverAddr = InetAddress.getByName(IP_SERVER);
+
+            mSocket = new Socket(serverAddr,SERVER_PORT);
+            pw = new PrintWriter(mSocket.getOutputStream());
+
+            Log.println(Log.VERBOSE,"socket","conectado al server");
+
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+
+        }
+
+    }
+
+    public class sendMessageThread extends Thread {
+        private String msg;
+
+        public sendMessageThread(String message) {
+            this.msg = message;
+        }
+
+        @Override
+        public void run() {
+            pw.write(msg);
+            pw.flush();
+            Log.println(Log.VERBOSE,"message","mensage enviado");
+        }
+    }
+
+
+
+}
